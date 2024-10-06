aeaee7aa2b4f59990cf61f8cf6c9a0754a0b26c6
Sebastian Wagner <swagner-de@users.noreply.github.com>
2024-10-06 15:26:42 +0200

    Initial code draft

---
+++ ldap-helper.py
@@ -0,0 +1,166 @@
+#! /usr/bin/env python3
+import base64
+import logging
+import os
+import sys
+from typing import Optional, Tuple
+
+import click
+import ldap3
+from ldap3.utils.conv import escape_filter_chars
+from ldap3.core.exceptions import LDAPInvalidCredentialsResult
+from flask import Flask, request, Response
+
+
+app = Flask(__name__)
+
+LOG = logging.getLogger(__name__)
+formatter = logging.Formatter('%(levelname)s %(asctime)s %(message)s')
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(formatter)
+LOG.addHandler(handler)
+
+LDAP_HELPER = None
+
+class LdapHelper:
+
+    def __init__(self, host:str , bind_dn: str, bind_pw: str, base_dn: str, ldap_filter: str, ssl=False):
+        self.server = ldap3.Server(host, use_ssl=ssl, get_info=ldap3.NONE)
+        self.host = host
+        self.bind_dn = bind_dn
+        self.bind_pw = bind_pw
+        self.base_dn = base_dn
+        self.ldap_filter = ldap_filter
+        con = self.connect()
+        con.unbind()
+    
+    def connect(self) -> ldap3.Connection:
+        return ldap3.Connection(self.server, self.bind_dn, self.bind_pw, auto_bind=True, raise_exceptions=True)
+
+
+    def search(self, user: str) -> Tuple[Optional[str], ldap3.Connection]:
+        user = escape_filter_chars(user)
+        con = self.connect()
+        filter_args = dict(username=user)
+        con.search(self.base_dn, self.ldap_filter.format(**filter_args))
+        result = con.entries
+        if len(result) == 0:
+            LOG.debug('No result found for filter %s' % (self.ldap_filter.format(**filter_args)))
+            LOG.info(('No result found for filter args %s' % (filter_args)))
+            return None, con
+        if len(result) > 1:
+            LOG.debug('Found more than one entry for filter %s' % (self.ldap_filter.format(**filter_args)))
+            LOG.info(('Found more than one entry for filter args %s' % (filter_args)))
+            return None, con
+        user_dn = result[0].entry_dn
+        return user_dn, con
+
+    def auth(self, con, user_dn, password):
+        try:
+            con.rebind(user=user_dn, password=escape_filter_chars(password))
+            LOG.debug("Username %s with supplied password is valid" % (user_dn))
+            return True
+        except LDAPInvalidCredentialsResult:
+            LOG.warning("Username %s credentials were incorrect" % (user_dn))
+            return False
+
+@app.route('/ldap-auth', methods=['GET'])
+def ldap_auth():
+    
+    auth = request.headers.get('Authorization')
+
+    if not auth or not auth.startswith('Basic '):
+        return Response(
+                'Could not verify your login!\n',
+                401,
+                {'WWW-Authenticate': 'Basic realm="Login Required"'}
+            )
+    
+    try:
+        base64_credentials = auth.split(' ')[1]
+        decoded_credentials = base64.b64decode(base64_credentials).decode('utf-8')
+        username, password = decoded_credentials.split(':')
+    except Exception as e:
+        return Response('Invalid request format\n', status=400)
+    
+    helper: LdapHelper = LDAP_HELPER
+    dn, con = helper.search(username)
+    if not dn:
+        con.unbind()
+        return Response('Forbidden\n', status=403)
+
+    if not helper.auth(con, dn, password):
+        return Response('Forbidden\n', status=403)
+    else:
+        return Response('Login successful\n', status=200)
+
+        
+@click.group()
+@click.option('--host', '-h', type=str, required=True,
+              help="LDAP server to connect")
+@click.option('--bind-dn', type=str, required=True,
+              help='DN used to make the initial bind and search for the to be authenticated user')
+@click.option('--bind-dn-password', type=str, required=True,
+              help='Password for the bind dn user')
+@click.option('--base-dn', type=str, required=True,
+              help="Base DN to use when looking up users")
+@click.option('--ldap-filter', type=str, required=True,
+              help="Filter for LDAP search to identify the right group membership")
+@click.option('--ssl', is_flag=True, default=False,
+              help="Use SSL connection")
+@click.option('--log-level', type=click.Choice(['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']),
+              default='INFO', help='Log level')
+@click.pass_context
+def group(ctx, host, bind_dn, bind_dn_password, base_dn, ldap_filter, ssl, log_level):
+    LOG.setLevel(log_level)
+    helper = LdapHelper(host, bind_dn, bind_dn_password, base_dn, ldap_filter, ssl)
+    ctx.ensure_object(dict)
+    ctx.obj.update({'helper': helper})
+
+
+@group.command()
+@click.option('--user', '-u', type=str, required=True,
+              help='User to look up')
+@click.pass_context
+def search(ctx, user):
+    helper: LdapHelper = ctx.obj['helper']
+    _, con = _search(helper, user)
+    con.unbind()
+
+def _search(helper, user):
+    result, con = helper.search(user)
+    if result:
+        print(f'Success finding user {user}, dn {result}')
+    else:
+        print(f"User {user} not found")
+    return result, con
+
+@group.command()
+@click.option('--user', '-u', type=str, required=True,
+              help='User to look up')
+@click.option('--password', type=str, required=True)
+@click.pass_context
+def auth(ctx, user, password):
+    helper: LdapHelper = ctx.obj['helper']
+    dn, con = _search(helper, user)
+    helper.auth(con, dn, password)
+
+
+@group.command()
+@click.option('--unix-socket', required=False,
+              help="Run on a unix socket")
+@click.option('--listen-address', default="127.0.0.1", help="Address to bind on")
+@click.option('--port', default="5000", help="Port to bind on")
+@click.pass_context
+def run_server(ctx, unix_socket, listen_address, port):
+    global LDAP_HELPER
+    LDAP_HELPER = ctx.obj['helper']
+    if unix_socket:
+        if os.path.exists(unix_socket):
+            os.remove(unix_socket)
+        app.run(host=f'unix://{unix_socket}')
+    else:
+        app.run(host=listen_address, port=port)
+
+if __name__ == "__main__":
+    group(auto_envvar_prefix='HA_LDAP')
